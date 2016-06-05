579ff283637d74300fb18ccfb14e104ff831898e
Juan Andres Garcia C <JAGC@imac-de-juan.local>
2016-06-05 10:01:36 -0500

    Initial Commit

---
+++ Reto Hamburguesas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reto Hamburguesas
+//
+//  Created by Juan Andres Garcia C on 5/06/16.
+//  Copyright © 2016 Juan Andres Garcia C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
